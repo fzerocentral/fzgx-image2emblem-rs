3093836fc9fed183a61bb51824481c6152efd8f0
Ricardo Mendes <rokusu@gmail.com>
2015-10-15 04:48:59 +0100

    Removes unnecessary parens in range

--- src/image.rs
+++ src/image.rs
@@ -10,7 +10,7 @@ pub fn resize(img: &mut self::image::DynamicImage) -> self::image::DynamicImage 
 }
 
 pub fn trim_edges(img: &mut self::image::DynamicImage) {
-    for i in (0..64) {
+    for i in 0..64 {
         (*img).put_pixel( i,  0, self::image::Rgba([0u8, 0u8, 0u8, 0u8]));
         (*img).put_pixel( i, 63, self::image::Rgba([0u8, 0u8, 0u8, 0u8]));
         (*img).put_pixel( 0,  i, self::image::Rgba([0u8, 0u8, 0u8, 0u8]));
